daacb31bfd14ef9c6fe3579a7b85107ae4899be2
Loupetre <62932842+Loupetre@users.noreply.github.com>
2025-02-20 20:44:37 +0100

    Update flash_memory.sh

--- flash_memory.sh
+++ flash_memory.sh
@@ -1,6 +1,6 @@
 if [ -e MEMORY_ZT3_Modded.bin ]
 then
-    	openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "reset halt" -c "flash probe 0" -c "stm32f1x unlock 0" -c "reset halt" -c "stm32f1x mass_erase 0" -c "flash write_bank 0 MEMORY_ZT3Pro_modded.bin" -c "reset run" -c "exit"
+    	openocd -s oocd/scripts -f oocd/scripts/interface/stlink.cfg -f oocd/scripts/target/at32.cfg -c "init" -c "reset halt" -c "flash probe 0" -c "stm32f1x unlock 0" -c "reset halt" -c "stm32f1x mass_erase 0" -c "flash write_bank 0 MEMORY_ZT3_modded.bin" -c "reset run" -c "exit"
 else
-    echo "Fichier MEMORY_ZT3Pro_Modded.bion inexistant"
+    echo "Fichier MEMORY_ZT3_Modded.bion inexistant"
 fi
